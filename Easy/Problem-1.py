e5dabec6cce725558df904a098b0323a6547d315
Shravan Kumar S <shravan.km.in@gmail.com>
2026-07-06 22:30:42 +0530

    Problem 1

---
+++ Easy/Problem-1.py
@@ -0,0 +1,69 @@
+#Question 1
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You can return the answer in any order.
+
+ 
+
+#Example 1:
+
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#Example 2:
+
+#Input: nums = [3,2,4], target = 6
+#Output: [1,2]
+#Example 3:
+
+#Input: nums = [3,3], target = 6
+#Output: [0,1]
+
+#Answer:
+
+nums = [2,7,11,15]
+target = 9
+l=len(nums)
+i=0
+c1=[]
+c2=[]
+while i<l:
+    j=i+1
+    while j<l:
+        if nums[i]+nums[j]==target:
+            c1=c1+[i]
+            c2=c2+[j]
+            break
+        j=j+1
+    i=i+1
+print(c1+c2)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
