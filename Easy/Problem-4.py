20b853496655bf675f141ef7f33c5340db742b7a
Shravan Kumar S <shravan.km.in@gmail.com>
2026-07-19 11:28:03 +0530

    Completed Problem 4

--- Easy/Problem-4.py
+++ Easy/Problem-4.py
@@ -16,3 +16,29 @@ Output: ""
 Explanation: There is no common prefix among the input strings.
 '''
 #Answer
+strs = ["dog","racecar","car"]
+i=0
+n=0
+k=0
+f=''
+while i<len(min(strs, key=len)):
+    j=0
+    c=strs[0][k]
+    while j<len(strs):
+        if c==strs[j][i]:
+            n=n+1
+        j=j+1
+    if n==len(strs):
+        f=f+strs[0][i]
+    n=0
+    i=i+1
+    k=k+1
+print(f)
+    
+            
+        
+        
+        
+        
+        
+    
